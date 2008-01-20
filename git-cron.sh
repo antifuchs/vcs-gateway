c7a51456f06fe880150fc3b78d0577e5ee2703d6
SBCL-GIT gateway and benchmark user <sbcl-arch@baker.localdomain>
2008-01-20 13:49:08 +0100

    boinkor.net CVS->git gateway scripts

---
+++ git-cron.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+
+
+BASE="$(cd $(dirname $0); pwd)"
+export BASE
+
+# lockfile
+lockfile-create "$BASE"/git.lock
+lockfile-touch "$BASE"/git.lock &
+TOUCHER="$!"
+
+trap 'kill $TOUCHER' EXIT 
+trap "lockfile-remove $BASE/git.lock" EXIT 
+
+
+# args
+VERBOSE="$1"
+verbosep(){
+	if [ "$VERBOSE" = "-v" ] || [ "$VERBOSE" = "--verbose" ] ; then
+		return 0
+	fi
+	return 1
+}
+
+verbosep && shift
+
+# run the converters
+
+for converter in "$BASE/converters-enabled/"* ; do 
+	converter_name="$(basename $converter)"
+	if ! [ -x $converter ] ; then 
+		echo "git-cron: skipping non-executable $converter_name"
+		continue 
+	fi
+
+	verbosep && echo "git-cron: running $converter_name"
+	if $converter ; then
+		verbosep && echo "git-cron: $converter_name succeeded."
+	else
+		echo "git-cron: $converter_name failed with exit status $?."
+	fi
+done
+
+# clean up after git-cvsimport
+rm -f /tmp/git*.cvsps 2>/dev/null
